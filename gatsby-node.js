f48ffafc1c96f6cf7822afdbe26b903bac2acf8c
Jonny Pickard <jonathandpickard@gmail.com>
2021-07-25 15:30:04 +0100

    Testing github pages docs folder

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,10 +4,6 @@ const fs = require("fs");
 exports.onPreInit = () => {
   if (process.argv[2] === "build") {
     fs.rmdirSync(path.join(__dirname, "docs"), { recursive: true });
-    fs.renameSync(
-      path.join(__dirname, "public"),
-      path.join(__dirname, "public_dev")
-    );
   }
 };
 
