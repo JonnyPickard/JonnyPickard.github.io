61df3c51ab55613ad560fdbe149e4b823085ae39
JonnyPickard <Jonathandpickard@gmail.com>
2023-07-31 10:48:05 +0000

    Deploying to main from  @ c48e4aac57c20945aec2a2a9c487214aa40a9734 🚀

---
+++ docs/assets/Introduction-8737e703.js
@@ -0,0 +1,10 @@
+import{j as t}from"./jsx-runtime-ffb262ed.js";import{M as r}from"./index-54225814.js";import{u as i}from"./index-a1cf9e47.js";import"./index-76fb7be0.js";import"./_commonjsHelpers-de833af9.js";import"./iframe-a292b367.js";import"../sb-preview/runtime.js";import"./index-d475d2ea.js";import"./_commonjs-dynamic-modules-302442b1.js";import"./index-d893ea64.js";import"./assert-a1982797.js";import"./index-d37d4223.js";import"./index-356e4a49.js";function n(e){const o=Object.assign({h1:"h1",h2:"h2",p:"p",blockquote:"blockquote",strong:"strong"},i(),e.components);return t.jsxs(t.Fragment,{children:[t.jsx(r,{title:"Introduction"}),`
+`,t.jsx(o.h1,{id:"jonny-pickard",children:"Jonny Pickard"}),`
+`,t.jsx(o.h2,{id:"introduction",children:"Introduction"}),`
+`,t.jsx(o.p,{children:"I've decided that the best way to show off a portfolio of examples is by using a component library. For this, I've chosen Storybook."}),`
+`,t.jsx(o.p,{children:"As I've been in full-time employment for a considerable amount of time, the examples currently visible on my Github are fairly dated."}),`
+`,t.jsx(o.p,{children:"My thoughts are that, by compartmentalizing using a component library, I will be able to gradually chip away at a portfolio, providing a more accurate representation of the work I'm capable of."}),`
+`,t.jsxs(o.blockquote,{children:[`
+`,t.jsxs(o.p,{children:["Storybook helps you build UI components in isolation from your app's business logic, data, and context. That makes it easy to develop hard-to-reach states. Save these UI states as ",t.jsx(o.strong,{children:"stories"})," to revisit during development, testing, or QA. - Storybook"]}),`
+`]})]})}function j(e={}){const{wrapper:o}=Object.assign({},i(),e.components);return o?t.jsx(o,Object.assign({},e,{children:t.jsx(n,e)})):n(e)}export{j as default};
+//# sourceMappingURL=Introduction-8737e703.js.map
