9e56da631e4369ae4ff4cefee5020d108850924b
Jonny Pickard <Jonathandpickard@gmail.com>
2024-01-15 15:19:01 +0000

    Threejs (#4)
    * feat: workout how to do react three js animations
    
    * docs: add docs on 3D animations using three.js with React
    
    * docs: update README.md

--- src/examples/react-threejs/Scene.stories.tsx
+++ src/examples/react-threejs/Scene.stories.tsx
@@ -1,17 +1,17 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
-import { Page } from "./Page";
+import { Scene } from "./Scene";
 
 const meta = {
-  title: "Examples/Page",
-  component: Page,
+  title: "Examples/ReactThreeJs/FlairAnimation",
+  component: Scene,
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
     layout: "fullscreen",
   },
-} satisfies Meta<typeof Page>;
+} satisfies Meta<typeof Scene>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const LoggedOut: Story = {};
+export const FlairAnimation: Story = {};
