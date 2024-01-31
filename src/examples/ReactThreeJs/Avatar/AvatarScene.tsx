12a439b4729bd608d7b1daab294b8db42913bbaa
Jonny Pickard <Jonathandpickard@gmail.com>
2024-01-31 13:21:12 +0000

    Game dev/redo avatar animation (#8)
    * feat: add new model for Avatar
    
    * feat: redo scene + docs

--- src/examples/ReactThreeJs/Avatar/AvatarScene.tsx
+++ src/examples/ReactThreeJs/Avatar/AvatarScene.tsx
@@ -1,24 +1,36 @@
 import { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { Canvas, useThree } from "@react-three/fiber";
+import { OrbitControls, CubeCamera, useCubeTexture } from "@react-three/drei";
 import { AvatarModel } from "./AvatarModel";
+
+const skyboxAssetPath = "/3d-models/textures/space-skybox/";
+
+function SkyBox() {
+  const { scene } = useThree();
+  const envMap = useCubeTexture(
+    ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg"],
+    { path: skyboxAssetPath },
+  );
+
+  // Set the scene background property to the resulting texture.
+  scene.background = envMap;
+  return null;
+}
 
 export const AvatarScene = () => {
   return (
     <Canvas
-      camera={{ position: [2, 0, 12.25], fov: 15 }}
       style={{
-        backgroundColor: "#111a21",
         width: "100vw",
         height: "100vh",
       }}
     >
       <ambientLight intensity={1.25} />
-      <ambientLight intensity={0.1} />
-      <directionalLight intensity={0.4} />
+      <directionalLight position={[0, 10, 5]} intensity={20} />
       <Suspense fallback={null}>
-        <AvatarModel position={[0, -0.4, -0.4]} />
+        <CubeCamera>{() => <AvatarModel position={[0, -1, 0]} />}</CubeCamera>
       </Suspense>
+      <SkyBox />
       <OrbitControls />
     </Canvas>
   );
