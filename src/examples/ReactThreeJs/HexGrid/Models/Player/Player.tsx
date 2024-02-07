f04db583c9208a18a1ec3acdc3130d3a82fed2d1
Jonny Pickard <Jonathandpickard@gmail.com>
2024-02-07 14:55:34 +0000

    feat: intermediary commit tweaking animations

--- src/examples/ReactThreeJs/HexGrid/Models/Player/Player.tsx
+++ src/examples/ReactThreeJs/HexGrid/Models/Player/Player.tsx
@@ -26,19 +26,33 @@ interface GLTFAction extends THREE.AnimationClip {
 
 export function Player(props: JSX.IntrinsicElements["group"]) {
   const group = useRef<THREE.Group>(null);
+
+  // NOTE: Loaders are cached so it should only run once
   const { nodes, materials, animations } = useGLTF(
-    "/3d-models/hex-grid/Player.glb",
+    // "/3d-models/hex-grid/Player.glb",
+    "/3d-models/hex-grid/Eve1.glb",
   ) as GLTFResult;
   const { actions } = useAnimations(animations, group);
 
   useEffect(() => {
+    // const srf = actions["Standing Run Forward"];
+
+    // if (srf) {
+    //   srf.repetitions = 0;
+    //   srf.clampWhenFinished = true;
+    //   srf.play();
+    // }
     actions["Standing Idle"]?.play();
-  });
+  }, [actions]);
 
   return (
     <group ref={group} {...props} dispose={null}>
       <group name="Scene">
-        <group name="Armature" rotation={[Math.PI / 2, 0, 0]} scale={0.01}>
+        <group
+          name="Armature"
+          rotation={[Math.PI / 2, 0, THREE.MathUtils.degToRad(30)]}
+          scale={0.01}
+        >
           <primitive object={nodes.mixamorigHips} />
           <skinnedMesh
             castShadow
@@ -53,4 +67,5 @@ export function Player(props: JSX.IntrinsicElements["group"]) {
   );
 }
 
-useGLTF.preload("/3d-models/hex-grid/Player.glb");
+// useGLTF.preload("/3d-models/hex-grid/Player.glb");
+useGLTF.preload("/3d-models/hex-grid/Eve1.glb");
