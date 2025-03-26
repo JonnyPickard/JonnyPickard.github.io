07e7f7c551023ad9760562dc129a89a419c9688a
Jonny Pickard <Jonathandpickard@gmail.com>
2025-03-26 12:25:12 +0000

    Remove comment

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,6 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // eslint-disable-next-line
-  // @ts-ignore
   test: {
     passWithNoTests: true,
     globals: true,
